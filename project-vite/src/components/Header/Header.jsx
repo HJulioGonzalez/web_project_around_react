e7b5213c515ba34e96b36abb446b61e7129c50a0
Habib Julio <89555113+HJulioGonzalez@users.noreply.github.com>
2025-08-07 11:39:31 +0930

    Sprint 14 - "final project, sending for Tripleten suggestions fixed"

--- project-vite/src/components/Header/Header.jsx
+++
@@ -1,8 +0,0 @@
-export default function Header() {
-  return (
-    <header className="header">
-      <p className="header__text">Around the World</p>
-      <p className="header__text-country">The Australian Territory SA</p>
-    </header>
-  );
-}
